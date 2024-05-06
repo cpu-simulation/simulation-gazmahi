fc4f7dd9a6e58ba488ca09be3a0172fb3af97e9e
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-05-06 15:05:08 +0330

    IMP => more job on models

--- src/models.py
+++ src/models.py
@@ -1,4 +1,44 @@
+"""
+HELP:
+write classes ypu need in here.
+i've been writing some of them but they are not completed
+for example:
+    write a class 'Core' with inheritance from BaseCore 
+    dont forget to import it in __init__.py file as Core
+
+"""
+
+
 from abc import ABC, abstractmethod
 
-class BaseCore(ABC):
+class BaseMemory(ABC):
     ...
+
+class BaseRegister(ABC):
+    ...
+
+
+class BaseCore(ABC):
+
+    memory : BaseMemory = ...
+    registers: BaseRegister = ... 
+
+    @abstractmethod
+    def set_register(self):
+        ...
+    
+    @property
+    @abstractmethod
+    def read_registers(self):
+        ...
+
+    @abstractmethod
+    def set_memory(self):
+        ...
+    
+    @property
+    @abstractmethod
+    def read_memory(self):
+        ...
+
+
