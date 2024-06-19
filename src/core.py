343587e27279acc267a260b3c3de8bf260615937
Alieza Ahmadi Veshki <alirezaahmadiveshki@gmail.com>
2024-06-19 22:24:28 +0330

    initilize compiler

--- src/core.py
+++ src/core.py
@@ -6,7 +6,7 @@ from pprint import pprint
 class Core:
     def __init__(self) -> None:
         self.memory = []
-        self.registers = [{"AC": 0X0000}, {"IR": 0X0000}, {"AR": 0X000}, {"DR": 0X0000}, {"INPR": 0X00}, {"PC": 0X000}, {"OUTR": 0X00}, {"TR": 0X0000}, {"E": 0}]
+        self.registers = [{"AC": "0X0000"}, {"IR": "0X0000"}, {"AR": "0X000"}, {"DR": "0X0000"}, {"INPR": "0X00"}, {"PC": "0X000"}, {"OUTR": "0X00"}, {"TR": "0X0000"}, {"E": "0"}]
 
 
     def memory_write(self, data: dict) -> None:
@@ -52,7 +52,89 @@ class Core:
 
     
     def compile(self, instructions: list[str]) -> None:
-    
+        machine_code_instruction = []
+        for instruct in instructions:
+                if "ADD" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X0" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0X8" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "AND" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X1" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0X9" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "LDA" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X2" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0XA" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "STA" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X3" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0XB" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "BUN" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X4" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0XC" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions") 
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "BSA" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X5" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0XD" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+                if "ISZ" in instruct:
+                    try:
+                        ar = instruct[6: ]
+                        if instruct[0] == "0":
+                            machine_code_instruction.append("0X4" + ar)
+                        elif instruct[0] == "1":
+                            machine_code_instruction.append("0XF" + ar) 
+                        else:
+                            raise ValueError("Wrong Instructions")
+                    except Exception:
+                        raise ValueError("Wrong Instructions")
+
+                    
+        return machine_code_instruction
+                    
     
     def execute_instruction(self) -> None:
         """
@@ -67,8 +149,9 @@ class Core:
 
 def main():
     c = Core()
-    c.memory_bulk_write([{0XFE: 0X12}, {0XFF: 0X13}])
+    c.memory_bulk_write([{"0XFE": "0X12"}, {"0XFF": "0X13"}])
     c.memory_bulk_read()
+    print(c.compile(["0 ADD 74f", "1 BSA 54C", "0 ISZ 001"]))
 
     
 
